30eb9d9fa8f0a61cb37db9d166242d1167bc9494
MajkenPlugge <MajkenPlugge@141.72.136.229>
2015-09-14 13:37:05 +0200

    er diagramm

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application
 	@Override
 	public void start(Stage stage) throws IOException 
 	{
-		Parent root = FXMLLoader.load(getClass().getResource("table.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("spielfeld.fxml"));
 	    
 		Scene scene = new Scene(root);
         stage.setTitle("FXML Welcome");
