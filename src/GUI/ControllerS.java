ecf7b141d52c8513300928ced61d3a4a8c17d780
caramaria <cara.damm@gmail.com>
2015-09-21 16:12:25 +0200

    Trying to get the label stuff working

--- src/GUI/ControllerS.java
+++ src/GUI/ControllerS.java
@@ -38,16 +38,41 @@ public class ControllerS
 Button button;
 
 @FXML
-Label lab00, lab01;
-
-
+Label lab00, lab01, lab02 ,lab03, lab04, lab05;
+@FXML
+Label lab10, lab11, lab12 ,lab13, lab14, lab15;
+@FXML
+Label lab20, lab21, lab22 ,lab23, lab24, lab25;
+@FXML
+Label lab30, lab41, lab42 ,lab43, lab44, lab45;
+@FXML
+Label lab50, lab51, lab52 ,lab53, lab54, lab55;
+@FXML
+Label lab60, lab61, lab62 ,lab63, lab64, lab65;
 
 public void clicked()
 {
 	Image imageY = new Image(getClass().getResourceAsStream("coinYel.png"));
 	Image imageR = new Image(getClass().getResourceAsStream("coinRed.png"));
 
-	lab00.setGraphic(new ImageView(imageY));
-	lab01.setGraphic(new ImageView(imageR));
-}
+	
+	String lab ="lab";
+	
+
+	for (int i = 0; i <3; i++) 
+	{
+		for (int j = 0; j < 4; j++) 
+		{
+			String label = lab+i+j;
+			Label l = new Label("");
+			System.out.println(l.);
+			l.setGraphic(new ImageView(imageR));
+		}//end of first for
+	}// end of secound for
+	
+	//lab00.setGraphic(new ImageView(imageY));
+	//lab01.setGraphic(new ImageView(imageR));
+	
+}// end of clicked()
+
 }// END OF class
