14083d635f77eaa5b88492ae68f6a6f5eb6f50cb
BjoernKorioth <bkorioth@gmx.de>
2015-09-28 15:07:12 +0200

    update

--- src/ki/Test_class.java
+++ src/ki/Test_class.java
@@ -14,49 +14,35 @@ public class Test_class {
 		feld [2][0] = 0;
 		feld [3][0] = 0;
 		feld [4][0] = 0;
-		feld [5][0] = 0;
+		feld [5][0] = 1;
 		
 		feld [0][1] = 0;
 		feld [1][1] = 0;
 		feld [2][1] = 0;
 		feld [3][1] = 0;
-		feld [4][1] = 0;
-		feld [5][1] = 0;
+		feld [4][1] = 1;
+		feld [5][1] = 2;
 		
 		feld [0][2] = 0;
 		feld [1][2] = 0;
 		feld [2][2] = 0;
-		feld [3][2] = 0;
-		feld [4][2] = 0;
-		feld [5][2] = 0;
+		feld [3][2] = 1;
+		feld [4][2] = 2;
+		feld [5][2] = 2;
 		
 		feld [0][3] = 0;
 		feld [1][3] = 0;
 		feld [2][3] = 0;
-		feld [3][3] = 0;
-		feld [4][3] = 0;
-		feld [5][3] = 0;
-		
-		feld [0][3] = 0;
-		feld [1][3] = 0;
-		feld [2][3] = 0;
-		feld [3][3] = 0;
-		feld [4][3] = 0;
-		feld [5][3] = 0;
-		
-		feld [0][3] = 0;
-		feld [1][3] = 0;
-		feld [2][3] = 0;
-		feld [3][3] = 0;
-		feld [4][3] = 0;
-		feld [5][3] = 0;
+		feld [3][3] = 1;
+		feld [4][3] = 1;
+		feld [5][3] = 2;
 		
 		feld [0][4] = 0;
 		feld [1][4] = 0;
 		feld [2][4] = 0;
-		feld [3][4] = 1;
-		feld [4][4] = 1;
-		feld [5][4] = 1;
+		feld [3][4] = 0;
+		feld [4][4] = 0;
+		feld [5][4] = 0;
 		
 		feld [0][5] = 0;
 		feld [1][5] = 0;
@@ -71,6 +57,8 @@ public class Test_class {
 		feld [3][6] = 0;
 		feld [4][6] = 0;
 		feld [5][6] = 0;
+		
+	
 		
 		
 		
