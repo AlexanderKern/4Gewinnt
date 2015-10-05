7c2d284932628e45e1fd095b00b270baf97f9317
AlexanderKern <alexander@spedition-kern.com>
2015-10-05 15:22:57 +0200

    KI + Pusher

---
+++ src/Database/Satzgewinner.java
@@ -0,0 +1,13 @@
+package Database;
+
+public class Satzgewinner {
+	private static String gewinner;
+	
+	public void setGewinner(String gewinner){
+		Satzgewinner.gewinner = gewinner;
+	}
+	
+	public String getGewinner(){
+		return gewinner;
+}
+}
---
+++ src/com/viergewinnt/api/pusher/PusherMain.java
@@ -0,0 +1,81 @@
+package com.viergewinnt.api.pusher;
+
+import java.awt.Toolkit;
+import java.io.IOException;
+import org.codehaus.jackson.map.ObjectMapper;
+import com.pusher.client.Pusher;
+import com.pusher.client.channel.PrivateChannel;
+import com.pusher.client.connection.Connection;
+import com.pusher.client.connection.ConnectionState;
+import com.viergewinnt.api.common.util.Function;
+import com.viergewinnt.api.common.util.Message;
+
+import Database.Satzgewinner;
+import ki.KI2;
+
+public class PusherMain {
+	private String team;
+
+	public PusherMain(String team){
+		this.team = team;
+	}
+
+	public String pusher() {
+		KI2 ki = new KI2();
+		
+		final PusherConnectionHandler pch = new PusherConnectionHandler()
+				.registerHandler("MoveToAgent", new Function <Pusher,PrivateChannel,String>(){
+					public void execute(Pusher pusher,PrivateChannel channel,String data) throws IOException {
+						//Parsen
+						ObjectMapper mapper = new ObjectMapper();
+						Message message = mapper.readValue(data, Message.class);
+						if(message.getFreigabe() && message.getSatzstatus().equals("Satz spielen") && message.getSieger().equals("offen") ){
+							if(Integer.parseInt(message.getGegnerzug())< 0){
+								ki.berechne();
+								channel.trigger("client-event", "{\"move\":\"" + ki.get_spalte() + "\"}");
+								ki.setStein(ki.get_spalte(), false);
+								//GUI und Datenbank Zug speichern weil björn cool
+								
+								
+							}
+								else{
+									ki.setStein(Integer.parseInt(message.getGegnerzug()), true);
+									//GUI und Datenbank Zug speichern weil björn cool
+									ki.berechne();
+									channel.trigger("client-event", "{\"move\":\"" + ki.get_spalte() + "\"}");
+									ki.setStein(ki.get_spalte(), false);
+									//GUI und Datenbank Zug speichern weil björn cool
+								}
+								
+							
+						}else{
+							Satzgewinner gewinner = new Satzgewinner();
+							gewinner.setGewinner(message.getSieger());
+							pusher.disconnect();
+						}
+						System.out.println(message.getFreigabe());
+						Toolkit.getDefaultToolkit().beep();
+						
+					}
+				});
+		
+		final Connection con = pch.connect();
+
+		System.out.println(con.getState().name());
+
+		// Connection zu Pusher offen halten.
+		do {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		} while (con.getState() == ConnectionState.CONNECTED || con.getState() == ConnectionState.CONNECTING);
+		
+		Satzgewinner gewinner = new Satzgewinner();
+		
+		return gewinner.getGewinner();
+
+	}
+
+}
